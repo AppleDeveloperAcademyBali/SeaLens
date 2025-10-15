38969e5b62006b52d68016cce4d9cb10a8e7d32e
shreyasvenadan <s3944652@student.rmit.edu.au>
2025-10-15 17:44:12 +0800

    inital tuist generation

---
+++ Project.swift
@@ -0,0 +1,30 @@
+import ProjectDescription
+
+let project = Project(
+    name: "SeaLens",
+    targets: [
+        .target(
+            name: "SeaLens",
+            destinations: .macOS,
+            product: .app,
+            bundleId: "dev.tuist.SeaLens",
+            infoPlist: .default,
+            buildableFolders: [
+                "SeaLens/Sources",
+                "SeaLens/Resources",
+            ],
+            dependencies: []
+        ),
+        .target(
+            name: "SeaLensTests",
+            destinations: .macOS,
+            product: .unitTests,
+            bundleId: "dev.tuist.SeaLensTests",
+            infoPlist: .default,
+            buildableFolders: [
+                "SeaLens/Tests"
+            ],
+            dependencies: [.target(name: "SeaLens")]
+        ),
+    ]
+)
---
+++ SeaLens/Tests/SeaLensTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import SeaLens
+
+struct SeaLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
