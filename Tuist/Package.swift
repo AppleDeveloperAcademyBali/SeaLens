c8e64787b2758e6e68f6d2491dbe6a1e1774978e
Shreyas Venadan <105024953+shreyasvenadan@users.noreply.github.com>
2025-10-17 15:01:48 +0800

    fix: ZipArchive dependency added to Package.swift

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -15,8 +15,7 @@ import PackageDescription
 let package = Package(
     name: "SeaLens",
     dependencies: [
-        // Add your own dependencies here:
-        // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
-        // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+        // Add your external Swift package dependencies here
+        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.6.0")
     ]
 )
