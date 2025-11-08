49ee0d191a258b659a423359243081c7fc5d069e
Handy <handy.kwok@gmail.com>
2025-11-08 19:14:37 +0800

    move the swift data container to SwiftDataService

---
+++ SeaLens/Shared/Services/SwiftDataService/SwiftDataService.swift
@@ -0,0 +1,42 @@
+//
+//  SwiftDataService.swift
+//  SeaLens
+//
+//  Created by Handy Handy on 08/11/25.
+//
+
+import Foundation
+import SwiftData
+
+public final class SwiftDataService {
+    
+    let container: ModelContainer
+    
+    static let shared = SwiftDataService()
+
+    private init () {
+        do {
+            let schema = Schema([
+                Footage.self,
+                FootageTags.self,
+                FishFamily.self,
+                Fish.self,
+                FishConfidenceScore.self,
+                FishFamilyReference.self,
+                FishSpeciesReference.self,
+                Site.self,
+                Location.self,
+                Transect.self
+            ])
+            
+            let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+            
+            self.container = try ModelContainer(for: schema, configurations: [config])
+        }
+        catch {
+            print(error)
+            fatalError("Failed to initialize ModelContainer \(error)")
+        }
+    }
+    
+}
