cca3fcd35e8672dac7ae296cf1d0b989f4b8b3c7
Handy <handy.kwok@gmail.com>
2025-11-15 15:04:40 +0800

    change ci post clone script from tuist docs

--- ci_post_clone.sh
+++ ci_post_clone.sh
@@ -6,17 +6,7 @@
 #  Created by Handy Handy on 15/11/25.
 #
 
-#!/bin/bash
-set -euxo pipefail
+#!/bin/sh
+brew install --formula tuist@x.y.z
 
-# Install Tuist (if you use the recommended installer)
-curl -Ls https://install.tuist.io | bash
-
-# Make sure tuist is in PATH
-export PATH="$HOME/.tuist/bin:$PATH"
-
-# Verify
-tuist version
-
-# Generate the Xcode project / workspace
-tuist generate --no-open
+tuist generate
